c1b7f73b613b0ca8faed8648445845614a311d8e
Demayl <denis.kanchev@storpool.com>
2025-04-08 15:32:13 +0300

    mock plugin base path sub

--- tlib/PVE/Storage/Plugin.pm
+++ tlib/PVE/Storage/Plugin.pm
@@ -1,3 +1,41 @@
 package PVE::Storage::Plugin;
 
+sub get_subdir {
+    my ($class, $scfg, $vtype) = @_;
+
+    my $path = $scfg->{path};
+
+    die "storage definition has no path\n" if !$path;
+    die "unknown vtype '$vtype'\n" if !exists($vtype_subdirs->{$vtype});
+
+    my $subdir = $scfg->{"content-dirs"}->{$vtype} // $vtype_subdirs->{$vtype};
+
+    return "$path/$subdir";
+}
+
+sub filesystem_path {
+    my ($class, $scfg, $volname, $snapname) = @_;
+
+    my ($vtype, $name, $vmid, undef, undef, $isBase, $format) =
+    $class->parse_volname($volname);
+
+    # Note: qcow2/qed has internal snapshot, so path is always
+    # the same (with or without snapshot => same file).
+    die "can't snapshot this image format\n"
+    if defined($snapname) && $format !~ m/^(qcow2|qed)$/;
+
+    my $dir = $class->get_subdir($scfg, $vtype);
+
+    $dir .= "/$vmid" if $vtype eq 'images';
+
+    my $path = "$dir/$name";
+
+    return wantarray ? ($path, $vmid, $vtype) : $path;
+}
+
+sub path {
+    my ($class, $scfg, $volname, $storeid, $snapname) = @_;
+
+    return $class->filesystem_path($scfg, $volname, $snapname);
+} 
 1;
